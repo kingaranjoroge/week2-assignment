849f5cdf0db342b2691eb92b28e68b29ee91fc9d
kingaranjoroge <briankingara4@gmail.com>
2025-03-28 19:49:05 +0300

    submit week2 assignment

---
+++ data-structures.py
@@ -0,0 +1,27 @@
+my_list = []
+
+# Appending elements to list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# Inserting element to list
+my_list.insert(1, 15)
+print(my_list)
+
+# Extending an array to list
+my_list.extend([50,60,70])
+print(my_list)
+
+# Removing the last element from list
+my_list.pop()
+print(my_list)
+
+# Sorting list in ascending order
+my_list.sort()
+print(my_list)
+
+# Finding and printing index of value 30
+print(my_list.index(30))
